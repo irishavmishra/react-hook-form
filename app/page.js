b936883201c7da27be500ee7ebb94ad2e4c76603
Rishav Mishra <irishavmishra@proton.me>
2024-12-14 02:28:38 +0530

    error fix and add css

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import { z } from "zod";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
   email: z.string().email(),
@@ -29,28 +30,46 @@ export default function Home() {
   };
 
   return (
-    <div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="flex  gap-2 text-black"
+        className="w-full max-w-sm bg-white p-6 rounded-lg shadow-md"
       >
-        <input {...register("email")} type="text" placeholder="Email" />
+        <input
+          {...register("email")}
+          type="text"
+          placeholder="Email"
+          className="w-full px-3 py-2 border text-black border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+        />
         {errors.email && (
-          <div className="text-red-500">{errors.email.message}</div>
+          <div className="text-sm text-red-500 mt-1">
+            {errors.email.message}
+          </div>
         )}
         <input
           {...register("password")}
           type="password"
           placeholder="Password"
+          className="w-full px-3 py-2 mt-4 border text-black border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
         />
         {errors.password && (
-          <div className="text-red-500">{errors.password.message}</div>
+          <div className="text-sm text-red-500 mt-1">
+            {errors.password.message}
+          </div>
         )}
-        <button type="submit" className="bg-white">
+
+        <button
+          type="submit"
+          className={`w-full py-2 px-4 mt-4 text-white rounded-lg ${
+            isSubmitting ? "bg-gray-500" : "bg-blue-500 hover:bg-blue-600"
+          }`}
+        >
           {isSubmitting ? "Loading..." : "Submit"}
         </button>
         {errors.root && (
-          <div className="text-red-500">{errors.root.message}</div>
+          <div className="text-sm text-red-500 mt-2 text-center">
+            {errors.root.message}
+          </div>
         )}
       </form>
     </div>
